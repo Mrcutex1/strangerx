635fd1b4658617793fa489f63bf8ab76d3a02250
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-07-12 11:48:53 +0530

    config.py

--- config/config.py
+++ config/config.py
@@ -49,7 +49,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sparrow9616/StrangerMusicBot",
+    "https://github.com/Mrcutex1/Strangerx",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
