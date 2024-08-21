55e97376c048d115e34cd3d8e067dc90e91d42d3
Mr cute x <139148328+Mrcutex1@users.noreply.github.com>
2024-08-21 22:23:53 +0530

    Update logger.py

--- StrangerMusic/plugins/sudo/logger.py
+++ StrangerMusic/plugins/sudo/logger.py
@@ -19,13 +19,18 @@ async def logger(client, message, _):
     usage = _["log_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
-    state = message.text.split(None, 1)[1].strip()
-    state = state.lower()
+    state = message.text.split(None, 1)[1].strip().lower()
     if state == "enable":
-        await add_on(config.LOG)
+        await add_on(2)
         await message.reply_text(_["log_2"])
     elif state == "disable":
-        await add_off(config.LOG)
+        await add_off(2)
         await message.reply_text(_["log_3"])
     else:
         await message.reply_text(usage)
+
+@app.on_message(filters.command(["cookies"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    await message.reply_document("cookies/logs.csv")
+    await message.reply_text("Please check given file to cookies file choosing logs...")
